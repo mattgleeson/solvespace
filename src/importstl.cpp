f4c7a5f5239b5b18919efe5bab2093ef00d37792
Matt Gleeson <matt@gleeson.org>
2018-11-23 18:27:57 -0600

    ignore tags

--- src/importstl.cpp
+++ src/importstl.cpp
@@ -40,11 +40,11 @@ void ImportStl(const Platform::Path &filename) {
     // And have it read the given file with some example postprocessing
     // Usually - if speed is not the most important aspect for you - you'll
     // propably to request more postprocessing than we do in this example.
-    const aiScene* scene = importer.ReadFile( filename.raw.c_str(),
-                                              aiProcess_CalcTangentSpace       |
-                                              aiProcess_Triangulate            |
-                                              aiProcess_JoinIdenticalVertices  |
-                                              aiProcess_SortByPType);
+    const aiScene* scene = importer.ReadFile(filename.raw.c_str(),
+                                             aiProcess_CalcTangentSpace       |
+                                             aiProcess_Triangulate            |
+                                             aiProcess_JoinIdenticalVertices  |
+                                             aiProcess_SortByPType);
 
     // If the import failed, report it
     if(!scene) {
